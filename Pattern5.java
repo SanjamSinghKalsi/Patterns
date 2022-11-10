57ff200a054914d4686f6565353cff96d119342e
SanjamSinghKalsi <80076219+SanjamSinghKalsi@users.noreply.github.com>
2022-11-10 13:12:15 +0530

    Create Pattern5.java

---
+++ Pattern5.java
@@ -0,0 +1,20 @@
+* * * * *
+* * * * 
+* * * 
+* *  
+* 
+  
+  
+class Solution {
+
+    void printTriangle(int n) {
+       while(n>0){
+             for(int i = n;i>=1;i--){
+            System.out.print("* ");
+        }
+        n--;
+        System.out.println("");
+        }
+       
+    }
+}
