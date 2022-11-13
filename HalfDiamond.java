01fe23d4adf03dd0bb7d58a1169990a42d24a23e
SanjamSinghKalsi <80076219+SanjamSinghKalsi@users.noreply.github.com>
2022-11-13 11:59:55 +0530

    Create HalfDiamond.java

---
+++ HalfDiamond.java
@@ -0,0 +1,38 @@
+   
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+* * * *
+* * *
+* *
+*
+
+void printTriangle(int n) {
+         int incre = 1;
+        int temp = n;
+        while(n>0){
+           
+            for(int i=1;i<=incre;i++){
+            System.out.print("* ");
+        }
+        System.out.println("");
+        n--;
+        incre++;
+        }
+         //System.out.println(n);
+         int down =1;
+         while(temp>0){
+            
+         
+             for(int i=1;i<temp;i++){
+                 System.out.print("* ");
+             }
+              System.out.println("");
+                 temp--;
+              down++;
+         }
+       
+        
+    }
